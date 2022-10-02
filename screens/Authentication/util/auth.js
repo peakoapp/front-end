063c979dad8c1eb6f12671900437700c4c915074
yuehan <62973650+Yuehan-Wang@users.noreply.github.com>
2022-10-02 16:09:57 -0500

    Update auth.js

--- screens/Authentication/util/auth.js
+++ screens/Authentication/util/auth.js
@@ -1,3 +1,25 @@
+// import axios from 'axios';
+
+// const API_KEY = '';
+
+// async function authenticate(mode, email, password) {
+//   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+//   const response = await axios.post(url, {
+//     email: email,
+//     password: password,
+//     returnSecureToken: true,
+//   });
+
+//   const token = response.data.idToken;
+//   return token;
+// }
+
+function authenticate(mode, email, password){
+  const token = '123456789'
+
+  return token
+}
 export async function createUser(email, password) {
     await authenticate('signUp', email, password);
   }
