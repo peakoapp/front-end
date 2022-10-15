ebbf05bf8d96a062e8d453188c5c56f7124e9d02
Yuehan Wang <yuehan@uchicago.edu>
2022-10-15 01:15:14 -0500

    login fetch api needs more work

--- screens/Authentication/util/auth.js
+++ screens/Authentication/util/auth.js
@@ -1,30 +1,29 @@
-// import axios from 'axios';
-
-// const API_KEY = '';
-
-// async function REALauthenticate(mode, email, password) {
-//   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
-
-//   const response = await axios.post(url, {
-//     email: email,
-//     password: password,
-//     returnSecureToken: true,
-//   });
-
-//   const token = response.data.idToken;
-//   return token;
-// }
-
-function authenticate(mode, email, password){
-  const token = '123456789'
-
-  return token
+async function authenticate(mode, email, password) {
+  url = "http://localhost:8080/v1/auth/signin";
+  if (mode == "signInWithPassword") {
+    await fetch(url, {
+      method: "POST",
+      body: JSOM.stringify({
+        email: email,
+        password: password,
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        return res.json.body.access_key;
+      })
+      .catch((err) => console.log(err));
+  }
 }
 
 export async function createUser(email, password) {
-  return await authenticate('signUp', email, password);
+  return await authenticate("signUp", email, password);
 }
-  
+
 export async function login(email, password) {
-  return await authenticate('signInWithPassword', email, password);
+  console.log("hi");
+  return await authenticate("signInWithPassword", email, password);
 }
